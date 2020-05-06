a48ab3d206918156a609a644aede4e86495082b7
cod3breaker <ravisherath@gmail.com>
2020-05-06 23:44:00 +1000

    animating the arrow button

--- Landmarks/HikeView.swift
+++ Landmarks/HikeView.swift
@@ -32,7 +32,9 @@ struct HikeView: View {
                     Image(systemName: "chevron.right.circle")
                         .imageScale(.large)
                         .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .scaleEffect(showDetail ? 1.5 : 1)
                         .padding()
+                        .animation(.easeInOut)
                 }
             }
 
